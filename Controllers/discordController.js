64e68d5309b59e0370aae9e9b0fedde56c5f3069
Robson-Vess <61565672+robsonvess55@users.noreply.github.com>
2021-07-26 17:50:36 -0300

    funcionalidade de se cadastrar no bot

--- Controllers/discordController.js
+++ Controllers/discordController.js
@@ -1,8 +1,11 @@
+const axios = require('axios');
+
 module.exports = {
     test : test,
     notFound : notFound,
     randomImage : randomImage,
-    dmTest : dmTest
+    dmTest : dmTest,
+    register : register
 };
 let duels = [];
 function test (msg) {
@@ -19,4 +22,17 @@ function randomImage (msg) {
 
 function dmTest(msg) {
     msg.author.send('Greetings traveler!');
+}
+
+function register(msg) {
+    axios.post(process.env.API_URL+'discord/register', {
+        'discord_id' : msg.author.id
+    }).then(response => {
+        console.log(response)
+        msg.author.send(response.data);
+    })
+    .catch(error => {
+        msg.author.send('Houve um erro na requisição');
+        console.log(error);
+    });
 }
--- server.js
+++ server.js
@@ -25,6 +25,9 @@ client.on('message', msg => {
       case 'dm' :
         discordController.dmTest(msg);
         break;
+      case 'register' :
+        discordController.register(msg);
+        break;
       case 'idtest' :
         //msg.reply(msg.mentions.users.entries().next().value[1].send('teste testado'));
         //console.log(msg.mentions.users.entries().next().value[0]);
